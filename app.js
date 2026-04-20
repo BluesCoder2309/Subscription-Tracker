9cd338068cc6c97cd2f11d6c418ec744789c6efe
Utkarsh Singh <singh.utkarsh2309@gmail.com>
2026-04-21 01:39:05 +0530

    added sign-up and sign-in authentication with jwt

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import cookieParser from "cookie-parser";
 import { PORT } from "./config/env.js";
 
 import authRouter from "./routes/auth.routes.js";
@@ -8,7 +8,12 @@ import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
